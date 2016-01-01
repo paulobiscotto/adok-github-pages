ec17a58d8ea6adf58a2fe5659d22e90eb61b229e
paulobiscotto <paul@toucantoco.com>
2016-01-01 20:00:04 +0100

    adok is now in two language - english style

--- js/index.js
+++ js/index.js
@@ -116,6 +116,13 @@ function() {
 });
 
 
+$(function() {
+  $('a[href*=#]').on('click', function(e) {
+    e.preventDefault();
+    $('html, body').animate({ scrollTop: $($(this).attr('href')).offset().top}, 500, 'linear');
+  });
+});
+
 // $(function() {
 //   $('a[href*=#]').on('click', function(e) {
 //     e.preventDefault();
